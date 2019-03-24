c28610b9ed702b8ec2a53315a173717e948eee57
Raitis <pingy030799@gmail.com>
2019-03-24 11:53:12 +0200

    added a sqlite3 db

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'sqlite3'
 
+configure do 
+	@db = SQLite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "username" TEXT, "phone" TEXT, "datestamp" TEXT, "barber" TEXT, "color" TEXT)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
